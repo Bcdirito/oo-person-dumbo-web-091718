11b9a25b6866cb5e1ee1547ce2c83846d9706eb1
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 03:23:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -25,7 +25,7 @@ class Person
   def hygiene=(n)
     if @hygiene + n >= 10
       @hygiene = 10
-    elsif @hygiene <= 0
+    elsif @hygiene + n <= 0
       @hygiene = 0
     else
       @hygiene += n
